1f307ca26d1b8627f1fcbdbce6b478243c9ceb0b
Paul <you@example.com>
2025-11-24 12:06:52 +0000

    index usage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 import parser from './parser.js';
 import render from './render.js';
 
-const sample = `<note>
-<to>Tove</to>
-<from>Jani</from>
-<heading>Reminder</heading>
-<body>Don't forget me this weekend!</body>
-</note>`;
-
-let converted = parser(sample);
-
-console.log(converted);
+export { parser, render };
